b35c3564bfb1d82dad8130cb1aa946bfabfdcb00
arpeiks <arpeiks@gmail.com>
2022-05-02 12:44:40 +0100

    updated token server url

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseUrl = "http://localhost:8000";
+const baseUrl = "https://git-app-auth.herokuapp.com";
 
 export const authApi = createApi({
   reducerPath: "authApi",
